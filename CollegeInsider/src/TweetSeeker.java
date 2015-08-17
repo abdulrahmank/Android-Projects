ef9160117d7f747105db593055a8377f50d52b96
abdulrahmank <kadkab.abdul@gmail.com>
2015-08-17 23:36:21 +0530

    A simple program to search for the tweets others have tweeted. Using this as the basic code, education mining can be done.

---
+++ CollegeInsider/src/TweetSeeker.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+
+public class TweetSeeker {
+public static void main(String[] args) {
+	Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken("accesstoken1", "accesstoken2");
+    twitter.setOAuthConsumer("consumertoken", "useridtoken");
+    twitter.setOAuthAccessToken(accessToken);
+
+    try {
+        Query query = new Query("#daudkhan003");
+        QueryResult result;
+        result = twitter.search(query);
+        List<Status> tweets = result.getTweets();
+        for (Status tweet : tweets) {
+            System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+        }
+    }
+    catch (TwitterException te) {
+        te.printStackTrace();
+        System.out.println("Failed to search tweets: " + te.getMessage());
+        System.exit(-1);
+    }
+}
+}
